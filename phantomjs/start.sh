c39ade7be1612b155d00378f011a7b4b5e0bc5b7
Gabriel de Labachelerie <glabachel@gmail.com>
2014-08-06 20:51:55 +0200

    Manually register the phantomjs node into the selenium grid

--- phantomjs/start.sh
+++ phantomjs/start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-CONTAINER_IP=$(ip addr show dev eth0 | grep "inet " | awk '{print $2}' | cut -d '/' -f 1)
+/usr/bin/phantomjs --webdriver=5555 &
 
-/usr/bin/phantomjs --webdriver=${CONTAINER_IP}:5555 --webdriver-selenium-grid-hub=http://${SELENIUM_HUB_ADDR}
+SELENIUM_HUB_IP=$(echo $SELENIUM_HUB_ADDR | cut -d : -f 1)
+SELENIUM_HUB_PORT=$(echo $SELENIUM_HUB_ADDR | cut -d : -f 1)
+
+SELENIUM_NODE_IP=$(echo $SELENIUM_NODE_ADDR | cut -d : -f 1)
+SELENIUM_NODE_PORT=$(echo $SELENIUM_NODE_ADDR | cut -d : -f 1)
+
+curl -H "Content-Type: application/json" -d "{\"configuration\": {\"registerCycle\": 5000, \"hub\": \"http://${SELENIUM_HUB_ADDR}\", \"host\": \"${SELENIUM_NODE_IP}\", \"proxy\": \"org.openqa.grid.selenium.proxy.DefaultRemoteProxy\", \"maxSession\": 1, \"port\": ${SELENIUM_NODE_PORT}, \"hubPort\": ${SELENIUM_HUB_PORT}, \"hubHost\": \"${SELENIUM_HUB_IP}\", \"url\": \"http://${SELENIUM_NODE_ADDR}\", \"remoteHost\": \"http://${SELENIUM_NODE_ADDR}\", \"register\": true, \"role\": \"wd\"}, \"capabilities\": [{\"seleniumProtocol\": \"WebDriver\", \"platform\": \"LINUX\", \"browserName\": \"phantomjs\", \"version\": \"1.9.7\", \"maxInstances\": 1}]}" http://$SELENIUM_HUB_ADDR/grid/register
+
+wait $!
+
