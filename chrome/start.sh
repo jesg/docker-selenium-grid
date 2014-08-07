55f93ce85d3645858a820c99bbbe4c4846a05ee0
Gabriel de Labachelerie <gdelabachelerie@amadeus.com>
2014-08-07 15:48:39 +0200

    Downgrade selenium version because of https://code.google.com/p/selenium/issues/detail?id=7396

--- chrome/start.sh
+++ chrome/start.sh
@@ -4,7 +4,7 @@ Xvfb :10 -screen 5 1024x768x8 -ac &
 export DISPLAY=:10.5
 
 java -Djava.security.egd=file:/dev/urandom \
--jar /opt/selenium-server-standalone-2.42.2.jar \
+-jar /opt/selenium-server-standalone.jar \
 -role node \
 -hub http://${SELENIUM_HUB_ADDR}/grid/register \
 -remoteHost http://${SELENIUM_NODE_ADDR} \
