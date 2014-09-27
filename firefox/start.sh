df0aa93bdd5f4a99d7251a9e8781246efef317bf
jesg <savagejas@gmail.com>
2014-09-27 10:05:24 -0500

    add firefox node configuration
    This configuration only works if the selenium hub is running on the host.

--- firefox/start.sh
+++ firefox/start.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-Xvfb :10 -screen 5 1024x768x8 -ac &
-export DISPLAY=:10.5
+export DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
+export CONTAINER_IP=$(ip addr show dev eth0 | grep "inet " | awk '{print $2}' | cut -d '/' -f 1)
+
+vncserver :10
 
 java -jar /opt/selenium-server-standalone.jar \
 -role node \
+-hub http://${DOCKER_HOST_IP}:4444/grid/register \
+-remoteHost http://${CONTAINER_IP}:5555 \
 -browser browserName=firefox,maxInstances=1,platform=LINUX
